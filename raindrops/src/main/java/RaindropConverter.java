a3d88ab1ff959abe39a3c8cbc2c615a6bbaa1296
ssaavedraa <=>
2023-04-29 16:15:51 -0500

    raindrops solution

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,23 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String stringResult = "";
+
+        if (number % 3 == 0) {
+            stringResult += "Pling";
+        }
+
+        if (number % 5 == 0) {
+            stringResult += "Plang";
+        }
+
+        if (number % 7 == 0) {
+            stringResult += "Plong";
+        }
+
+        return stringResult.isEmpty()
+                ? String.valueOf(number)
+                : stringResult;
     }
 
 }
